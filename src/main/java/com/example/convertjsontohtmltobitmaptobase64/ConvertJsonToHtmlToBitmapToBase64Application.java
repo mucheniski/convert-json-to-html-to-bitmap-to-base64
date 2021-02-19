c73c3c1489f78e9730d58ff8b9afa18aa8611dc8
mucheniski <mucheniski@gmail.com>
2021-02-19 08:22:46 -0300

    first commit

---
+++ src/main/java/com/example/convertjsontohtmltobitmaptobase64/ConvertJsonToHtmlToBitmapToBase64Application.java
@@ -0,0 +1,13 @@
+package com.example.convertjsontohtmltobitmaptobase64;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertJsonToHtmlToBitmapToBase64Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertJsonToHtmlToBitmapToBase64Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/convertjsontohtmltobitmaptobase64/ConvertJsonToHtmlToBitmapToBase64ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.convertjsontohtmltobitmaptobase64;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertJsonToHtmlToBitmapToBase64ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
